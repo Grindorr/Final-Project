cd1fa3206c14ef206fccc191e3b94872d18d20cc
Grindorr <matheusgrinferreir@gmail.com>
2022-11-25 18:25:47 +0000

    Salvar

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-#incio Projeto
+#Importando a biblioteca
+from  flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+tasks = [
+    {'name':'Estudar', 'status': 'lido'},
+    {'name': 'Dormir', 'status': 'lido'},
+    {'name': 'comer', 'status': 'não lido'}
+]
+
+@app.route('/')
+def home():
+    # templates/home.html
+    return render_template('home.html', tasks=tasks)
+
+@app.route('/create', methods=['POST'])
+def create():
+    name = request.form['name']
+    status = request.form['status']
+    task = {'name': name, 'status': status}
+    tasks.append(task)
+    return render_template('home.html', tasks=tasks)
+
+
+app.run(debug=True)
